13dff339b8a967c3ae11d8aa65323da2637b410d
Priyanshu0007 <priyanshu2859@gmail.com>
2026-07-19 17:44:46 +0530

    Initial Commit

---
+++ TransitTrip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TransitTrip
+//
+//  Created by Priyanshu Gupta on 19/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TransitTrip/TransitTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitTripApp.swift
+//  TransitTrip
+//
+//  Created by Priyanshu Gupta on 19/07/26.
+//
+
+import SwiftUI
+
+@main
+struct TransitTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
