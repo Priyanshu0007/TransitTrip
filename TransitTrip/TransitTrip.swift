ed669122304a4c99b011fa95c213204718689855
Priyanshu0007 <priyanshu2859@gmail.com>
2026-07-20 10:13:25 +0530

    feat: implement TransitTrip model with arrival time calculation and vehicle type support

---
+++ TransitTrip/TransitTrip.swift
@@ -0,0 +1,26 @@
+//
+//  TransitTrip.swift
+//  TransitTrip
+//
+//  Created by Priyanshu Gupta on 19/07/26.
+//
+
+import Foundation
+
+struct TransitTrip: Identifiable, Codable{
+    let id: UUID
+    let lineName: String
+    let destination: String
+    let estimatedArrival: Date
+    let vehicleType: VehicleType
+    
+    enum VehicleType: String, Codable{
+        case metro
+        case bust
+    }
+    
+    var minutesRemaining: Int{
+        let difference = estimatedArrival.timeIntervalSince(Date())
+        return max(0, Int(difference/60))
+    }
+}
